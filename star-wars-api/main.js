88fad862d8c6b5613bbfc1b8230f8d8b9c30d553
Angeline Bennett <10944047@uvu.edu>
2020-10-27 00:39:49 -0600

    Got our data import

---
+++ star-wars-api/main.js
@@ -0,0 +1,18 @@
+import { films } from './data/films.js'
+import { people } from './data/people.js'
+
+//console.log(people.length)
+
+//console.log(films[0]) 
+
+films.forEach(film => {
+    let newParagraph = document.body.appendChild(document.createElement("p"))
+    newParagraph.textContent = film.title
+    
+})
+
+people.forEach(person => {
+    let newParagraph = document.body.appendChild(document.createElement("p"))
+    newParagraph.textContent = person.name
+    
+})
