fe21dafb81fda9836476fe72fa2e1b24eae5967a
Angeline Bennett <10944047@uvu.edu>
2020-10-30 20:38:00 -0600

    Added functions

---
+++ star-wars-characters/main.js
@@ -0,0 +1,82 @@
+import { people } from '../data/people.js'
+import { starships } from '../data/starships.js'
+
+const mainContent = document.querySelector('#main')
+
+const mainHeader = document.createElement('header')
+mainHeader.className = 'mainHeader'
+document.body.insertBefore(mainHeader, mainContent)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+mainHeader.appendChild(femaleButton)
+
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
+mainHeader.appendChild(otherButton)
+
+const maleCharaters = people.filter(person => person.gender === 'male')
+
+const femaleCharaters = people.filter(person => person.gender === 'female')
+
+maleButton.addEventListener('click', () => populateDOM(maleCharaters))
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharaters))
+    
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach(element => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true)// genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+
+        mainContent.appendChild(charFigure)
+    })
+}
+//let theUrl = "https://swapi.co/api/people/2/"
+//let theUrl2 = "https://swapi.co/api/people/11/"
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if(url.charAt(start)=='/'){
+        start ++
+    }
+    return(url.slice(start, end))
+}
+function removeChildren(container) { 
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
