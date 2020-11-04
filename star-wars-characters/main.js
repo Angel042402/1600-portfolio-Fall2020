92d8e879d54fe67f57696ae143a12120c1f3f9db
Angeline Bennett <10944047@uvu.edu>
2020-11-04 16:09:51 -0700

    Added starships file

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -1,5 +1,4 @@
 import { people } from '../data/people.js'
-import { starships } from '../data/starships.js'
 
 const mainContent = document.querySelector('#main')
 
@@ -7,60 +6,58 @@ const mainHeader = document.createElement('header')
 mainHeader.className = 'mainHeader'
 document.body.insertBefore(mainHeader, mainContent)
 
-const starshipButton = document.createElement('button')
-starshipButton.textContent = 'Starfighters'
-mainHeader.appendChild(starshipButton)
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+mainHeader.appendChild(maleButton)
 
-const starshipDButton = document.createElement('button')
-starshipDButton.textContent = 'Death Star'
-mainHeader.appendChild(starshipDButton)
-
-const newDiv = document.createElement('div');
-newDiv.className = 'container'
-document.body.insertBefore(newDiv, mainContent)
-
-
-
-
-/*const femaleButton = document.createElement('button')
+const femaleButton = document.createElement('button')
 femaleButton.textContent = 'Female Characters'
 mainHeader.appendChild(femaleButton)
 
 const otherButton = document.createElement('button')
 otherButton.textContent = 'Other Characters'
-mainHeader.appendChild(otherButton)*/
+mainHeader.appendChild(otherButton)
 
-const starshipFighter = starships.filter(starships => starships.starship_class === `Starfighter`)
+const maleCharaters = people.filter(person => person.gender === 'male')
 
-const starshipDestroyer = starships.filter(starships => starships.starship_class === `Deep Space Mobile Battlestation`)
+const femaleCharaters = people.filter(person => person.gender === 'female')
 
-//const femaleCharaters = people.filter(person => person.gender === 'female')
+maleButton.addEventListener('click', () => populateDOM(maleCharaters))
 
+femaleButton.addEventListener('click', () => populateDOM(femaleCharaters))
 
-starshipButton.addEventListener('click', () => populateDOM(starshipFighter))
-starshipDButton.addEventListener('click', () => populateDOM(starshipDestroyer))
+/*const otherCharacters = people.filter((thing) => {
+    if (thing.gender === 'n/a'||
+        thing.gender === 'none'||
+        thing.gender === 'hermaphrodite') 
+        {
+        return thing
+    }
+})*/
 
 /*speciesButton.addEventListener('click', () => populateDOM(speciesClassification))*/
 
 //femaleButton.addEventListener('click', () => populateDOM(femaleCharaters))
     
-function populateDOM(starships) {
+function populateDOM(characters) {
     removeChildren(mainContent)
-    starships.forEach(element => {
-        const shipFigure = document.createElement('figure')
-        const shipImg = document.createElement('img')
-        let shipNum = getLastNumber(element.url)
-        shipImg.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-        shipImg.addEventListener('error', () => shipImg.hidden = true)// genius level
-        const shipCaption = document.createElement('figcaption')
-        shipCaption.textContent = element.name
+    characters.forEach(element => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true)// genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
 
-        shipFigure.appendChild(shipImg)
-        shipFigure.appendChild(shipCaption)
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
 
-        mainContent.appendChild(shipFigure)
+        mainContent.appendChild(charFigure)
     })
 }
+console.log("Hello There")
+
 //let theUrl = "https://swapi.co/api/people/2/"
 //let theUrl2 = "https://swapi.co/api/people/11/"
 
---
+++ star-wars-starships/main.js
@@ -0,0 +1,66 @@
+import { starships } from '../data/starships.js'
+
+const mainContent = document.querySelector('#main')
+
+const mainHeader = document.createElement('header')
+mainHeader.className = 'mainHeader'
+document.body.insertBefore(mainHeader, mainContent)
+
+const starshipButton = document.createElement('button')
+starshipButton.textContent = 'Starfighters'
+mainHeader.appendChild(starshipButton)
+
+const starshipDButton = document.createElement('button')
+starshipDButton.textContent = 'Death Star'
+mainHeader.appendChild(starshipDButton)
+
+/*const newDiv = document.createElement('div'); //created a new div, I want to use this div to be a container for the images I want to transition from one to another.
+newDiv.className = 'container'
+document.body.insertBefore(newDiv, mainContent)
+
+const newImg = document.createElement('img')// experimenting with ways to make one background img morph or fade into another
+newImg.className = 'background_img_darth'
+document.body.insertBefore(newImg, mainContent)*/
+
+const starshipFighter = starships.filter(starships => starships.starship_class === `Starfighter`)
+
+const starshipDestroyer = starships.filter(starships => starships.starship_class === `Deep Space Mobile Battlestation`)
+
+starshipButton.addEventListener('click', () => populateDOM(starshipFighter))
+
+starshipDButton.addEventListener('click', () => populateDOM(starshipDestroyer))
+
+function populateDOM(starships) {
+    removeChildren(mainContent)
+    starships.forEach(element => {
+        const shipFigure = document.createElement('figure')
+        const shipImg = document.createElement('img')
+        let shipNum = getLastNumber(element.url)
+        shipImg.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+        shipImg.addEventListener('error', () => shipImg.hidden = true)// genius level
+        const shipCaption = document.createElement('figcaption')
+        shipCaption.textContent = element.name
+
+        shipFigure.appendChild(shipImg)
+        shipFigure.appendChild(shipCaption)
+
+        mainContent.appendChild(shipFigure)
+    })
+}
+//let theUrl = "https://swapi.co/api/people/2/"
+//let theUrl2 = "https://swapi.co/api/people/11/"
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if(url.charAt(start)=='/'){
+        start ++
+    }
+    return(url.slice(start, end))
+}
+function removeChildren(container) { 
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+}
