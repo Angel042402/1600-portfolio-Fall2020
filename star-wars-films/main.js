08dd1fe59da631ec0e35045f8c03887bd4eb908b
Angeline Bennett <10944047@uvu.edu>
2020-11-13 20:02:49 -0700

    film captions corrected

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -1,15 +1,14 @@
 import { films } from '/data/films.js'
-
+import { getLastNumber } from '../utils/index.js'
 const main = document.querySelector('main')
 
 for (let step = 0; step < 7; step++) {
-    // Runs 7 times, with values of step 0 through 6.
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
     let figCaption = document.createElement('figcaption')
-    figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg` //set the source of it or nothing will show
-    //now append the image to the DOM somehow
-    figCaption.textContent = films[step].title
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg` //set it's source or nothing will show
+    const foundFilm = films.find(film => parseInt(getLastNumber(film.url)) === (step + 1))
+    figCaption.textContent = foundFilm.title
 
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
--- utils/index.js
+++ utils/index.js
@@ -12,3 +12,26 @@ export function getLastNumber(url) {
     }
     return url.slice(start, end)
 }
+
+/*function addStarField(element, numStars) {
+    for (let i = 0; i< numStars; i++){
+  star.style.setProperty('background-color', 'black')
+  star.style.setProperty('width', '2px')
+  star.style.setProperty('height', '2px')
+  star.style.setProperty('background-color', 'white')
+  let xy =getRandomPosition()
+  star.style.left = `${xy[0]}px`
+  star.style.top = `${xy[0]}px`
+  .appendChild(star)
+}
+}
+ function getRandomPosition(){
+let y =document.body.scrollHeight
+let x = document.body.scrollWidth
+let randomY = math.floor(math.random() * y)
+let randomX = math.floor(math.random() * x)
+return [randomY, randomX]
+
+
+ }
+ addStarField(document.querySelector('body'), 1000)*/
