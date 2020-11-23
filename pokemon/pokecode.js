f019bce81ab22e69b21f1bf7710256b72c276614
Angeline Bennett <10944047@uvu.edu>
2020-11-22 22:20:15 -0700

    Added card back image

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -26,30 +26,42 @@ function loadPage() {
 const pokemonGrid = document.querySelector('.pokemonGrid')
 let card = document.querySelector('.card')
 
-
 function populatePokeCard(pokemon) {
     let pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
     let pokeCard = document.createElement('div')
     pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => {
+        pokeCard.classList.toggle('is-flipped')
+    })
+    
+    pokeCard.appendChild(populateCardFront(pokemon))
+    pokeCard.appendChild(populateCardBack(pokemon))
+    pokeScene.appendChild(pokeCard)
+    pokemonGrid.appendChild(pokeScene)
+}
+
+ function populateCardFront(pokemon){
     let cardFront = document.createElement('div')
     cardFront.className = 'card__face card__face--front'
     let frontLabel = document.createElement('p')
     let frontImage = document.createElement('img')
+    frontLabel.textContent = pokemon.name
+    frontImage.src = `../personal-portfolio/images/pokemon/${getImageFileName(pokemon)}.png`
+    cardFront.appendChild(frontImage)
+    cardFront.appendChild(frontLabel)
+    frontLabel.textContent = `${pokemon.name}`
+    return cardFront
+ }
+
+ function populateCardBack(pokemon){
     let cardBack = document.createElement('div')
     cardBack.className = 'card__face card__face--back'
     let backLabel = document.createElement('p')
-
-    frontLabel.textContent = pokemon.name
-    frontImage.src = `../personal-portfolio/images/pokemon/${getImageFileName(pokemon)}.png`
     backLabel.textContent = `I'm the back of the card`
-    cardFront.appendChild(frontImage)
-    cardFront.appendChild(frontLabel)
     cardBack.appendChild(backLabel)
-    pokeCard.appendChild(cardFront)
-    pokeCard.appendChild(cardBack)
-    pokeScene.appendChild(pokeCard)
-    pokemonGrid.appendChild(pokeScene)
+    /*pokeCard.appendChild(cardBack)*/
+    return cardBack
 }
 
 function getImageFileName(pokemon) {
