67d3dc0ff7c5e4f5d43a701abfbc73a89139b42f
Angeline Bennett <10944047@uvu.edu>
2020-12-09 19:06:24 -0700

    fixed portfolio, added images

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -8,8 +8,6 @@ async function getAPIData(url) {
         console.error(error)
     }
 }
-
-
 // now, use the async getAPIData function
 function loadPage() {
     getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then
@@ -30,53 +28,23 @@ const loadButton = document.createElement('button')
 loadButton.textContent = 'Load Pokemon'
 pokeHeader.appendChild(loadButton)
 
-const morepokeButton = document.createElement('button')
-morepokeButton.textContent = 'load more Pokemon!'
-pokeHeader.appendChild(morepokeButton)
+const morePokeButton = document.createElement('button')
+morePokeButton.className = 'more-pokemon'
+morePokeButton.textContent = 'load more Pokemon!'
+pokeHeader.appendChild(morePokeButton)
 
 loadButton.addEventListener('click', () => {
     loadPage()
     loadButton.disabled = true //populates cards once then disables the load button
 })
 
-/*mudsDaleButton.addEventListener('click', () => {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/750`).then
-    (async (data) => {
-        let mudMoves = document.createElement('ul')
-        data.moves.forEach(move =>{
-            console.log(move.move.name)
-            let moveItem = document.createElement('li')
-            moveItem.textContent = move.move.name
-            mudMoves.appendChild(moveItem)
-        })
-        let mudImage = document.createElement('img')
-        mudImage.src = `../personal-portfolio/images/pokemon/750.png`
-        pokemonGrid.appendChild(mudMoves)
-        pokemonGrid.appendChild(mudImage)
-    })
-})*/
-
-
+morePokeButton.addEventListener('click', () => {
+    let pokeName = prompt("What's your new Pokemon's name?");
+    populatePokeCard(createNewPokemon(pokeName))
+})
 
 const pokemonGrid = document.querySelector('.pokemonGrid')
 let card = document.querySelector('.card')
-
-/*otherButton.addEventListener('click', () => populateDOM(otherShips))
-
-function populateDOM(starships) {
-    removeChildren(mainContent)
-    starships.forEach(element => {
-        let error = false
-        const shipFigure = document.createElement('figure')
-        const shipImg = document.createElement('img')
-        let shipNum = getLastNumber(element.url)
-        shipImg.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-        const shipCaption = document.createElement('figcaption')
-        shipCaption.textContent = element.name
-        shipImg.addEventListener('error', () => {
-            shipImg.hidden = true
-            shipCaption.hidden = true // genius level 
-        })*/
 
 function populatePokeCard(pokemon) {
     let pokeScene = document.createElement('div')
@@ -118,11 +86,13 @@ function populateCardBack(pokemon) {
         abilityName.textContent = ability.ability.name
         abilityList.appendChild(abilityName)
     })
-    let movesLabel =document.createElement('h3')
-    movesLabel.textContent = 'Most Accurate Move:'
+    let movesLabel = document.createElement('h3')
+    movesLabel.textContent = 'Stats:'
     let moveAccuracy = document.createElement('h4')
     //const mostAccurateMove = getBestAccuracyAndPower(pokemon.moves)
     //moveAccuracy.textContent = `${mostAccurateMove.move.name}`
+    let pokeWeight = document.createElement('h5')
+    pokeWeight.textContent = `Weight: ${pokemon.weight} lbs.`
     let backImage = document.createElement('img')
     backImage.className = 'card_back_image'
     backImage.src = '../personal-portfolio/images/pokemon/test_img.png'
@@ -130,21 +100,22 @@ function populateCardBack(pokemon) {
     cardBack.appendChild(abilityList)
     cardBack.appendChild(movesLabel)
     cardBack.appendChild(moveAccuracy)
+    cardBack.appendChild(pokeWeight)
     return cardBack
 }
 
-function getBestAccuracyAndPower(pokemoves){
+function getBestAccuracyAndPower(pokemoves) {
     return pokemoves.reduce((mostAccurate, move) => {
-    //console.log(move.move.url)
+        //console.log(move.move.url)
         getAPIData(move.move.url).then
-        (async (data) => {
-            /*let mudMoves = document.createElement('ul')
-            data.moves.forEach(move =>{*/
+            (async (data) => {
+                /*let mudMoves = document.createElement('ul')
+                data.moves.forEach(move =>{*/
                 console.log(data.accuracy, data.power)
                 /*let moveItem = document.createElement('li')
                 moveItem.textContent = move.move.name
                 mudMoves.appendChild(moveItem)*/
-            })  
+            })
         //return mostAccurate.accuracy > move.accuracy ? mostAccurate : move;
     }, {});
 }
@@ -156,13 +127,34 @@ function getImageFileName(pokemon) {
         return `0${pokemon.id}`
     }
 }
-function Pokemon(name, height, weight, abilities) {
+function Pokemon(name, height, weight, abilities, moves) {
     this.name = name
     this.height = height
     this.weight = weight
     this.abilities = abilities
     this.id = 900
+    this.moves = moves
 }
 
-let angelmon = new Pokemon('Angelmon', 62, 'unknown', ['junkfood-consumption', 'cat-whisperer', 'tired-always'])
+let angelmon = new Pokemon('Angelmon', 62, 'Uh, no. Just no', ['junkfood-consumption', 'cat-whisperer', 'most-excellent-grandma'])
 console.log(angelmon)
+
+function createNewPokemon(name) {
+    return new Pokemon(name, 62, 'Uh, no. Just no', ['junkfood-consumption', 'cat-whisperer', 'most-excellent-grandma'])
+}
+/*mudsDaleButton.addEventListener('click', () => {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/750`).then
+    (async (data) => {
+        let mudMoves = document.createElement('ul')
+        data.moves.forEach(move =>{
+            console.log(move.move.name)
+            let moveItem = document.createElement('li')
+            moveItem.textContent = move.move.name
+            mudMoves.appendChild(moveItem)
+        })
+        let mudImage = document.createElement('img')
+        mudImage.src = `../personal-portfolio/images/pokemon/750.png`
+        pokemonGrid.appendChild(mudMoves)
+        pokemonGrid.appendChild(mudImage)
+    })
+})*/
